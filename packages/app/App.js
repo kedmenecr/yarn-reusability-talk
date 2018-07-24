4460183fe40fe0942d7d51c4474a2e4077f9e1b0
kedmenecr <kedmenec.roberto@gmail.com>
2018-07-24 23:22:29 +0200

    Add support for react-native

--- packages/app/App.js
+++ packages/app/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { add } from '@yarn-reusability-talk/common'
 export default class App extends React.Component {
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text>Open up App.js to start working on your app!</Text>
-				<Text>Changes you make will automatically reload.</Text>
-				<Text>Shake your phone to open the developer menu.</Text>
+				<Text>{add(1, 3)}</Text>
+
 			</View>
 		);
 	}
---
+++ packages/app/link-workspaces.js
@@ -0,0 +1,4 @@
+require('crna-make-symlinks-for-yarn-workspaces')(__dirname)
+
+// take react native and expo and put it into main node_modules
+// make symlink
